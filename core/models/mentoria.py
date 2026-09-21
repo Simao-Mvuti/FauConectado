38a22a3e0e7a5e24f17f8e022eae291020301672
simao-mvuti <simaolusimbadiolutondamvuti@gmail.com>
2026-09-21 21:56:31 +0100

    corringo sempre os erros

--- core/models/mentoria.py
+++ core/models/mentoria.py
@@ -4,7 +4,6 @@ class Mentoria(models.Model):
     nome = models.CharField(max_length=100)
     curso = models.CharField(max_length=100)
     ano = models.IntegerField()
-    descricao = models.TextField()
     contacto_publico = models.CharField(max_length=50)
     avaliacao_media = models.FloatField(default=0.0)
     total_avaliacoes = models.IntegerField(default=0)
